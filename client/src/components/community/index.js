411ab08ff735c646b3e1cdbe8e2376dc4300f7f1
WaseemTalib <waseemltalib@gmail.com>
2020-05-07 20:20:29 +0500

    adeeb's files

--- client/src/components/community/index.js
+++ client/src/components/community/index.js
@@ -46,28 +46,28 @@ class index extends Component {
             return TopicList.slice(0, 2).map(i => {
                 return <li key={i._id} className="right-side-item">
                     <Link to="" className="right-side-link">
-                        <img src={i.image} alt="topic" className="right-side-img" />
+                        <img src={i.image} alt="topic" style={{ width: '30px', height: '30px', borderRadius: '1000px', marginRight: '16px' }} />
                         <span className="right-side-text">Dummy Course</span>
                     </Link>
                 </li>
-            })
+       })
     }
 
     render() {
         const { auth, profile, Feed, TopicList } = this.props;
-
-        return <div className="content">
+        return<div className="content">
 
             <nav className="sidebar ">
-                <h6 style={{ width: '100%', backgroundColor: '#f4f2f2', textAlign: 'center', fontWeight: 'bold', color: '#e74c3c', marginBottom: '0px' }} className="p-2"><span className="fa fa-feed mr-2"></span> Communities</h6>
+                <h6 onClick={()=>window.innerWidth < 1000 ? this.refs.dropdown.classList.toggle("display_on_short") : ""} style={{ width: '100%', backgroundColor: '#f4f2f2', textAlign: 'center', fontWeight: 'bold', color: '#e74c3c', marginBottom: '0px',padding: `20` }} className="p-2"><span className="fa fa-feed mr-2"></span> Communities</h6>
+                <div className={`hide_on_click ${window.innerWidth < 1000 ? "display_on_short" : ""}`} ref="dropdown">
                 <ul className="side-nav">
                     {this.renderCommunityLinks(TopicList)}
                 </ul>
                 <div className="side-more-images">
-                    <div alt="topic" className="side-more-img" style={{backgroundColor: "blue"}}>&ensp;</div>
-                    <div alt="topic" className="side-more-img" style={{backgroundColor: "green"}}>&ensp;</div>
-                    <div alt="topic" className="side-more-img" style={{backgroundColor: "red"}}>+20</div>
-                    <Link to="/morecommunities" className="side-more-text" style={{textDecoration:"none"}}>See More +</Link>
+                    <img src="./img/user-4.jpg" className="side-more-img" alt="" />
+                    <img src="./img/user-4.jpg" className="side-more-img" alt="" />
+                    <img src="./img/user-4.jpg" className="side-more-img" alt="" />
+                    <p className="side-more-text">See More +</p>
                 </div>
                 <hr className="side-nav-hr" />
                 <div className="side-social">
@@ -83,6 +83,15 @@ class index extends Component {
                         <li className="side-social-item"><a className="side-social-link" href="#"> Google Play Store</a></li>
                     </ul>
                 </div>
+                </div>
+                {window.innerWidth < 1000 ? <div style={{width: `100%`}}>
+                <div className="right-side-border">
+                <h2 className="right-side-heading-h"  onClick={()=>window.innerWidth < 1000 ? this.refs.courses.classList.toggle("display_on_short") : ""}>Recommended Courses</h2>
+                    <ul ref="courses" className={`right-side-list ${window.innerWidth < 1000 ? "display_on_short" : ""}`}>
+                        {this.renderCoursesLinks(TopicList)}
+                    </ul>
+                </div>
+            </div> : ""}
             </nav>
             <div className="center1">
                 {auth && profile && <Suspense fallback={<Fragment />}><AddPost profile={profile} list={TopicList} submit={this.submit} /></Suspense>}
@@ -90,16 +99,15 @@ class index extends Component {
                     <List list={Feed} />
                 </Suspense>
             </div>
-            <div className="right-side">
+            {window.innerWidth > 1000 ? <div className="right-side">
                 <div className="right-side-border">
-                    <h2 className="right-side-heading-h">Recommended Courses</h2>
+                <h2 className="right-side-heading-h">Recommended Courses</h2>
                     <ul className="right-side-list">
                         {this.renderCoursesLinks(TopicList)}
                     </ul>
                 </div>
+            </div> : ""}
             </div>
-        </div>
-
     }
 }
 
