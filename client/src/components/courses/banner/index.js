a720c48efc1cf55052d685b389972073f0c26b1f
WaseemTalib <waseemltalib@gmail.com>
2020-05-07 20:37:37 +0500

    adeeb's files

--- client/src/components/courses/banner/index.js
+++ client/src/components/courses/banner/index.js
@@ -6,6 +6,7 @@ class index extends Component{
         return (
             <div className="col-12 d-flex flex-row justify-content-center">
                 <div className="col-lg-7 col-12 banner">
+
                 </div>
             </div>
         )
